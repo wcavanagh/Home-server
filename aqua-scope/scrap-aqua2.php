02b7a2f9cf53c23e78421c878959c516b3acc759
Gracious Mashasha <graccasoft263@gmail.com>
2023-12-15 14:59:15 +0200

    Conform to firmware update

--- aqua-scope/scrap-aqua2.php
+++ aqua-scope/scrap-aqua2.php
@@ -1,56 +1,28 @@
 <?php
 
-require 'simple_html_dom.php';
 function secondsToTime($seconds) {
     $dtF = new \DateTime('@0');
     $dtT = new \DateTime("@$seconds");
     return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes');
 }
-$htmlString = file_get_contents(  'http://192.168.5.111');
-$html = str_get_html($htmlString);
 
-$ul = $html->find('ul',0);
-$ul->remove();
+$handle = curl_init();
+$url = "http://192.168.5.111/json";
+curl_setopt($handle, CURLOPT_URL, $url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 
-$div = $html->find('div',0);
-$statsText = trim( $div->plaintext );
+$output = curl_exec($handle);
 
-$lines = explode(PHP_EOL, $statsText);
-$cleanValues = [];
-foreach ($lines as $line){
-    $tmp = explode(":", $line);
-    if( count($tmp) == 2 ){
-        $key = trim( str_replace(" ","_", $tmp[0]) );
-        $cleanValues[ $key ] = trim($tmp[1]);
-        //clean
-        switch( strtolower($key) ){
-            case "pressure":{
-                $tmpValue = explode(" ",trim($tmp[1]));
-                $cleanValues[$key] = floatval($tmpValue[0])/1000 ;
-                break;
-            }
-
-            case "water_temperature":{
-                $tmpValue = explode(" ",trim($tmp[1]));
-                $cleanValues[$key] = floatval($tmpValue[0])/10 ;
-                break;
-            }
-
-            case "uptime":{
-                $tmpValue = explode(" ",trim($tmp[1]));
-                $cleanValues[$key] = secondsToTime($tmpValue[0]) ;
-                break;
-            }
-        }
-    }
-}
-
-//print_r( $cleanValues );
+$valuesObj = json_decode($output);
 $html = "<html><body>";
 
-foreach($cleanValues as $key => $value){
-    $html.="$key <span class='$key'>$value</span><br>";
-}
+$html.= "Uptime <span class='uptime'>".secondsToTime($valuesObj->uptime->value)." </span><br>";
+$html.= "Temperature <span class='uptime'>". floatval($valuesObj->temperature->value)/10 ."</span><br>";
+$html.= "Voltage <span class='uptime'>".$valuesObj->voltage->value." ". $valuesObj->voltage->scale ."</span><br>";
+$html.= "Battery <span class='uptime'>".$valuesObj->battery->value." ". $valuesObj->battery->scale ."</span><br>";
+$html.= "Pressure <span class='uptime'>". floatval($valuesObj->pressure->value)/1000 ."</span><br>";
+$html.= "Consumption <span class='uptime'>".$valuesObj->consumption->value." ". $valuesObj->consumption->scale." </span><br>";
+$html.= "Meter <span class='uptime'>".$valuesObj->meter->value ." ".$valuesObj->meter->scale." </span><br>";
 $html.="</body></html>";
 
 file_put_contents("device-2.html", $html);
